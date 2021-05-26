ac3962c721a1ef3c7f8db81b1e4f6c1127bba22f
alisdemir33 <52675649+alisdemir33@users.noreply.github.com>
2021-05-26 18:46:45 +0300

    usectx

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,28 +1,31 @@
-import React from 'react';
+import React, {useContext} from "react";
+import AuthContext from "../store/auth-context";
+import classes from "./Navigation.module.css";
 
-import classes from './Navigation.module.css';
-
-const Navigation = (props) => {
+const Navigation = (props) => {  
+   const ctx = useContext(AuthContext);
   return (
-    <nav className={classes.nav}>
-      <ul>
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Users</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Admin</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
+   
+          <nav className={classes.nav}>
+            <ul>
+              {ctx.isLoggedInCtx && (
+                <li>
+                  <a href="/">Users</a>
+                </li>
+              )}
+              {ctx.isLoggedInCtx && (
+                <li>
+                  <a href="/">Admin</a>
+                </li>
+              )}
+              {ctx.isLoggedInCtx && (
+                <li>
+                  <button onClick={props.onLogout}>Logout</button>
+                </li>
+              )}
+            </ul>
+          </nav>
+       
   );
 };
 
