98120c3f9af2c7ac645ede50631409d8414aa779
alisdemir33 <52675649+alisdemir33@users.noreply.github.com>
2021-05-28 15:26:48 +0300

    asd

--- src/App.js
+++ src/App.js
@@ -1,48 +1,21 @@
-import React, { useState,useEffect } from 'react';
+import React, { useContext } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
-import AuthContext from './components/store/auth-context'
+import AuthContext from './store/auth-context';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-
-  
-  useEffect( () => {
-    console.log('LOGIN EFFECT RUNNING');
-    const isLoggedInFromLocalStorage= localStorage.getItem('setLoggedIn')
-    if(isLoggedInFromLocalStorage==="1"){
-      setIsLoggedIn(true);
-    } 
-  
-    },[])
- 
-  
- 
- 
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-
-    localStorage.setItem('setLoggedIn','1');
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem('setLoggedIn');
-    setIsLoggedIn(false);
-  };
+  const ctx = useContext(AuthContext);
 
   return (
-    <AuthContext.Provider value={ {isLoggedInCtx:isLoggedIn}}>
-      <MainHeader  onLogout={logoutHandler} />
+    <React.Fragment>
+      <MainHeader />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
       </main>
-      </AuthContext.Provider>
+    </React.Fragment>
   );
 }
 
--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,31 +1,31 @@
-import React, {useContext} from "react";
-import AuthContext from "../store/auth-context";
-import classes from "./Navigation.module.css";
+import React, { useContext } from 'react';
 
-const Navigation = (props) => {  
-   const ctx = useContext(AuthContext);
+import AuthContext from '../../store/auth-context';
+import classes from './Navigation.module.css';
+
+const Navigation = () => {
+  const ctx = useContext(AuthContext);
+
   return (
-   
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedInCtx && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {ctx.isLoggedInCtx && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedInCtx && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-       
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={ctx.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
